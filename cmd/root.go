82b3e435270a6d3e8739c5ee50e1cb0f4f92dfb4
George Goldberg <george@appletreetechnology.com>
2023-12-30 14:13:49 +0000

    Basic CMD Line flags.

--- cmd/root.go
+++ cmd/root.go
@@ -14,6 +14,14 @@ var rootCmd = &cobra.Command{
 	Run:   Process,
 }
 
+var legalHoldData string
+var outputPath string
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&legalHoldData, "legal-hold-data", "", "Path to the legal hold data file")
+	rootCmd.PersistentFlags().StringVar(&outputPath, "output-path", "", "Path where the output files will be written")
+}
+
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -22,5 +30,10 @@ func Execute() {
 }
 
 func Process(cmd *cobra.Command, args []string) {
-	fmt.Println("Process a file...")
+	fmt.Println("Running the Mattermost Legal Hold Processor")
+	fmt.Printf("- Input data: %s\n", legalHoldData)
+	fmt.Printf("- Procesed output will be written to: %s\n", outputPath)
+	fmt.Println()
+	fmt.Println("Let's begin...")
+	fmt.Println()
 }
