a98f22be1b829aacc268eb352ea488d6c625c7e0
George Goldberg <george@appletreetechnology.com>
2024-02-04 17:15:20 +0000

    Complete tests for kvstore.

--- server/store/kvstore/legal_hold_test.go
+++ server/store/kvstore/legal_hold_test.go
@@ -24,8 +24,11 @@ func TestKVStore_CreateLegalHold(t *testing.T) {
 
 	// Test with a fresh legal hold
 	lh1 := model.LegalHold{
-		ID:   mattermostModel.NewId(),
-		Name: "legal-hold-1",
+		ID:          mattermostModel.NewId(),
+		Name:        "legal-hold-1",
+		DisplayName: "Legal Hold 1",
+		UserIDs:     []string{mattermostModel.NewId()},
+		StartsAt:    mattermostModel.GetMillis(),
 	}
 
 	api.On("KVSetWithOptions",
@@ -42,6 +45,17 @@ func TestKVStore_CreateLegalHold(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, lh1.ID, lh2.ID)
 	assert.Equal(t, lh1.Name, lh2.Name)
+	assert.Equal(t, lh1.UserIDs, lh2.UserIDs)
+	assert.Equal(t, lh1.StartsAt, lh2.StartsAt)
+	assert.Equal(t, lh1.EndsAt, lh2.EndsAt)
+	assert.Equal(t, lh1.DisplayName, lh2.DisplayName)
+	assert.Equal(t, lh1.LastExecutionEndedAt, lh2.LastExecutionEndedAt)
+	assert.Equal(t, lh1.ExecutionLength, lh2.ExecutionLength)
+	assert.Equal(t, int64(0), lh1.CreateAt)
+	assert.Equal(t, int64(0), lh1.UpdateAt)
+	assert.NotEqual(t, int64(0), lh2.CreateAt)
+	assert.NotEqual(t, int64(0), lh2.UpdateAt)
+	assert.Equal(t, lh2.CreateAt, lh2.UpdateAt)
 
 	// Test with a legal hold with duplicate ID
 	lh3 := model.LegalHold{
@@ -141,11 +155,90 @@ func TestKVStore_GetAllLegalHolds(t *testing.T) {
 }
 
 func TestKVStore_UpdateLegalHold(t *testing.T) {
-	// TODO: Implement me!
-	_ = t
+	api := &plugintest.API{}
+	driver := &plugintest.Driver{}
+	client := pluginapi.NewClient(api, driver)
+
+	kvstore := NewKVStore(client)
+
+	// Original legal hold
+	lh1 := model.LegalHold{
+		ID:          mattermostModel.NewId(),
+		Name:        "legal-hold-1",
+		DisplayName: "Legal Hold 1",
+		UserIDs:     []string{mattermostModel.NewId()},
+		StartsAt:    mattermostModel.GetMillis(),
+	}
+
+	// Update legal hold
+	lh2 := model.LegalHold{
+		ID:          lh1.ID,
+		Name:        "legal-hold-1",
+		DisplayName: "Legal Hold 2",
+		UserIDs:     []string{mattermostModel.NewId()},
+		StartsAt:    mattermostModel.GetMillis(),
+	}
+
+	api.On("KVSetWithOptions",
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("[]uint8"),
+		mock.AnythingOfType("model.PluginKVSetOptions"),
+	).Run(func(args mock.Arguments) {
+		marshaled := args.Get(1).([]uint8)
+		api.On("KVGet", mock.AnythingOfType("string")).Return(marshaled, nil).Once()
+	}).Return(true, nil).Once()
+
+	// Test updating a legal hold
+	lh3, err := kvstore.UpdateLegalHold(lh2, lh1)
+	require.NoError(t, err)
+	assert.Equal(t, lh3.ID, lh2.ID)
+	assert.Equal(t, lh3.Name, lh2.Name)
+	assert.Equal(t, lh3.UserIDs, lh2.UserIDs)
+	assert.Equal(t, lh3.StartsAt, lh2.StartsAt)
+	assert.Equal(t, lh3.EndsAt, lh2.EndsAt)
+	assert.Equal(t, lh3.DisplayName, lh2.DisplayName)
+	assert.Equal(t, lh3.LastExecutionEndedAt, lh2.LastExecutionEndedAt)
+	assert.Equal(t, lh3.ExecutionLength, lh2.ExecutionLength)
+	assert.NotEqual(t, lh3.UpdateAt, lh2.UpdateAt)
+	assert.Equal(t, lh3.CreateAt, lh2.UpdateAt)
+
+	// Test updating a legal hold that does not exist
+	lh4 := model.LegalHold{
+		ID:          "doesnotexist",
+		Name:        "legal-hold-4",
+		DisplayName: "Legal Hold 4",
+		UserIDs:     []string{mattermostModel.NewId()},
+		StartsAt:    mattermostModel.GetMillis(),
+	}
+	_, err = kvstore.UpdateLegalHold(lh4, *lh3)
+	require.Error(t, err)
 }
 
 func TestKVStore_DeleteLegalHold(t *testing.T) {
-	// TODO: Implement me!
-	_ = t
+	api := &plugintest.API{}
+	driver := &plugintest.Driver{}
+	client := pluginapi.NewClient(api, driver)
+
+	kvstore := NewKVStore(client)
+
+	// Test deleting a legal hold that exists
+	lhID := mattermostModel.NewId()
+	api.On("KVSetWithOptions",
+		fmt.Sprintf("%s%s", legalHoldPrefix, lhID),
+		mock.AnythingOfType("[]uint8"),
+		mock.AnythingOfType("model.PluginKVSetOptions"),
+	).Return(true, nil).Once()
+
+	err := kvstore.DeleteLegalHold(lhID)
+	require.NoError(t, err)
+
+	// Test deleting a legal hold that doesn't exist
+	api.On("KVSetWithOptions",
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("[]uint8"),
+		mock.AnythingOfType("model.PluginKVSetOptions"),
+	).Return(false, &mattermostModel.AppError{})
+
+	err = kvstore.DeleteLegalHold("does-not-exist")
+	require.Error(t, err)
 }
