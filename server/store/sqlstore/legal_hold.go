0afda796c5a9d13bf510e9b987356eadde8efdc1
Stuart Doherty <stu.doherty@gmail.com>
2024-06-14 14:26:34 -0400

    Modified channelmemberhistory reference to be CamelCase for DBs with case sensistive config (#65)

--- server/store/sqlstore/legal_hold.go
+++ server/store/sqlstore/legal_hold.go
@@ -113,7 +113,7 @@ func (ss SQLStore) GetPostsBatch(channelID string, endTime int64, cursor model.L
 func (ss SQLStore) GetChannelIDsForUserDuring(userID string, startTime int64, endTime int64, excludePublic bool) ([]string, error) {
 	query := ss.replicaBuilder.
 		Select("distinct(cmh.channelid)").
-		From("channelmemberhistory as cmh").
+		From("ChannelMemberHistory as cmh").
 		Where(sq.Lt{"cmh.jointime": endTime}).
 		Where(sq.Or{sq.Eq{"cmh.leavetime": nil}, sq.GtOrEq{"cmh.leavetime": startTime}}).
 		Where(sq.Eq{"cmh.userid": userID})
