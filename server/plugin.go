8ee1ca0de38f3d69b481f7daca20a7a5d4040595
Raghav Aggarwal <raghav.aggarwal@brightscout.com>
2024-10-03 01:30:24 +0530

    [GH-94] Disable S3 settings if AWS config is invalid (#98)
    * [GH-94] Disable AWS settings if AWS config is invalid

--- server/plugin.go
+++ server/plugin.go
@@ -174,6 +174,17 @@ func (p *Plugin) Reconfigure() error {
 	}
 
 	if err = filesBackend.TestConnection(); err != nil {
+		pluginConfig := p.Client.Configuration.GetPluginConfig()
+
+		// Disable the S3 settings in case the AWS config is invalid
+		pluginConfig["amazons3bucketsettings"].(map[string]interface{})["Enable"] = false
+
+		confErr := p.Client.Configuration.SavePluginConfig(pluginConfig)
+		if confErr != nil {
+			p.Client.Log.Error("Error while saving plugin config.", "Error", confErr.Error())
+			return confErr
+		}
+
 		err = errors.Wrap(err, "connection test for filestore failed")
 		p.Client.Log.Error(err.Error())
 		return err
