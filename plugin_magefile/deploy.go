aa5b7314db3fb90d36e459c1b1550b3422ad2262
Felipe Martin <me@fmartingr.com>
2025-02-20 09:52:34 +0100

    fix: also bundle on upload

--- plugin_magefile/deploy.go
+++ plugin_magefile/deploy.go
@@ -8,7 +8,7 @@ type Deploy mg.Namespace
 
 // Upload builds and installs the plugin to a server
 func (Deploy) Upload() error {
-	mg.SerialDeps(Build.All, Pluginctl.Deploy)
+	mg.SerialDeps(Build.All, Build.Bundle, Pluginctl.Deploy)
 
 	return nil
 }
