511d72ec0aaf19a60c582604a2d37c6859acabc9
David Krauser <david@krauser.org>
2025-03-17 11:05:49 -0400

    Add API to search for groups by display name

---
+++ server/store/sqlstore/group.go
@@ -0,0 +1,50 @@
+package sqlstore
+
+import (
+	"strings"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/pkg/errors"
+
+	"github.com/mattermost/mattermost-server/v6/model"
+)
+
+var escapeLikeSearchChar = []string{
+	"%",
+	"_",
+}
+
+func sanitizeSearchTerm(term string) string {
+	const escapeChar = "\\"
+
+	term = strings.ReplaceAll(term, escapeChar, "")
+
+	for _, c := range escapeLikeSearchChar {
+		term = strings.ReplaceAll(term, c, escapeChar+c)
+	}
+
+	return term
+}
+
+func (ss SQLStore) SearchGroupsByPrefix(prefix string) ([]*model.Group, error) {
+	sanitizedPrefix := strings.ToLower(sanitizeSearchTerm(prefix))
+	query := ss.replicaBuilder.
+		Select("Id", "DisplayName", "DeleteAt").
+		From("UserGroups").
+		Where(sq.Like{"LOWER(DisplayName)": sanitizedPrefix + "%"}).
+		Where(sq.Eq{"DeleteAt": 0}).
+		OrderBy("DisplayName").
+		Limit(10)
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to build SQL query for searching groups")
+	}
+
+	var groups []*model.Group
+	if err := ss.replica.Select(&groups, sql, args...); err != nil {
+		return nil, errors.Wrap(err, "failed to search groups")
+	}
+
+	return groups, nil
+}
