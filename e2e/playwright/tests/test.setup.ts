864e6117218a92a641dcea7a652b9e9557ac9df3
Felipe Martin <me@fmartingr.com>
2025-03-21 08:38:02 +0100

    test(e2e): send company_country when requesting trial license

--- e2e/playwright/tests/test.setup.ts
+++ e2e/playwright/tests/test.setup.ts
@@ -58,6 +58,7 @@ async function requestTrialLicense(adminClient: Client) {
             receive_emails_accepted: true,
             terms_accepted: true,
             users: 100,
+            company_country: 'e2e-ci-test',
         });
     } catch (e) {
         console.error('Failed to request trial license', e);
