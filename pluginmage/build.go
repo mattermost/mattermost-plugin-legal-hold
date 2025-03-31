3593b706636c62dd84d8fceb6f9773067ddec551
Felipe Martin <me@fmartingr.com>
2025-03-31 16:05:44 +0200

    chore: change default build platforms

--- pluginmage/build.go
+++ pluginmage/build.go
@@ -116,9 +116,6 @@ var (
 	DefaultPlatforms = []BuildPlatform{
 		{GOOS: "linux", GOARCH: "amd64"},
 		{GOOS: "linux", GOARCH: "arm64"},
-		{GOOS: "darwin", GOARCH: "amd64"},
-		{GOOS: "darwin", GOARCH: "arm64"},
-		{GOOS: "windows", GOARCH: "amd64"},
 	}
 
 	// AllBinaries holds all binaries that need to be built
