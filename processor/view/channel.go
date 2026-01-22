44800ab7ff52dc1914c1788ca2861a627c8e5ea8
Doug Lauder <wiggin77@warpmail.net>
2026-01-22 03:54:02 -0500

    fix: nil pointer crash in processor when user deleted from DM/GM (#154)

--- processor/view/channel.go
+++ processor/view/channel.go
@@ -9,6 +9,50 @@ import (
 	"github.com/mattermost/mattermost-plugin-legal-hold/processor/model"
 )
 
+// GetChannelAndTeamData retrieves channel and team data from lookups, or creates fallback data
+// for channels not in the index (e.g., Direct Messages or Group Messages that don't belong to teams).
+func GetChannelAndTeamData(channelID string, firstPost *model.Post, channelLookup model.ChannelLookup, teamForChannelLookup model.TeamForChannelLookup) (*model.LegalHoldChannel, *model.LegalHoldTeam) {
+	// Try to get channel data from lookup
+	channelData := channelLookup[channelID]
+	if channelData == nil && firstPost != nil {
+		// Use data from the first post for channels not in the index (DMs/GMs)
+		channelData = &model.LegalHoldChannel{
+			ID:          channelID,
+			Name:        firstPost.ChannelName,
+			DisplayName: firstPost.ChannelDisplayName,
+			Type:        firstPost.ChannelType,
+		}
+	} else if channelData == nil {
+		// Fallback if no posts exist - must be a DM/GM since it's not in a team
+		channelData = &model.LegalHoldChannel{
+			ID:          channelID,
+			Name:        "Direct/Group Message",
+			DisplayName: "Direct/Group Message",
+			Type:        "D",
+		}
+	}
+
+	// Try to get team data from lookup
+	teamData := teamForChannelLookup[channelID]
+	if teamData == nil && firstPost != nil {
+		// Use data from the first post for channels not in a team (DMs/GMs)
+		teamData = &model.LegalHoldTeam{
+			ID:          firstPost.TeamID,
+			Name:        firstPost.TeamName,
+			DisplayName: firstPost.TeamDisplayName,
+		}
+	} else if teamData == nil {
+		// Fallback for DMs/GMs which don't belong to a team
+		teamData = &model.LegalHoldTeam{
+			ID:          "",
+			Name:        "No Team",
+			DisplayName: "No Team",
+		}
+	}
+
+	return channelData, teamData
+}
+
 // WriteChannel takes the data for the posts in a channel and writes out the page for that channel.
 func WriteChannel(hold model.LegalHold, channel model.Channel, posts []*model.PostWithFiles, teamData *model.LegalHoldTeam, channelData *model.LegalHoldChannel, outputPath string) error {
 	data := struct {
@@ -94,9 +138,16 @@ func WriteUserAllChannels(hold model.LegalHold, user model.User, allPosts map[st
 	}
 
 	for channelID, posts := range allPosts {
+		// Get channel and team data from lookups, or create fallback if not found
+		var firstPost *model.Post
+		if len(posts) > 0 {
+			firstPost = posts[0].Post
+		}
+		channelData, teamData := GetChannelAndTeamData(channelID, firstPost, channelLookup, teamForChannelLookup)
+
 		data.Channels = append(data.Channels, ChannelData{
-			TeamData:    teamForChannelLookup[channelID],
-			ChannelData: channelLookup[channelID],
+			TeamData:    teamData,
+			ChannelData: channelData,
 			Posts:       posts,
 		})
 	}
